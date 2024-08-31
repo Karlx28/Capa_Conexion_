13f7f61672cd4ca298e4c1c2aa150067cc7c10e6
Karlx28 <mo0436032023@unab.edu.sv>
2024-08-30 19:44:09 -0600

    Guia nueve

--- Capa-Conexion/Capa-Conexion/Form1.cs
+++ Capa-Conexion/Capa-Conexion/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+
 namespace Capa_Conexion
 {
     public partial class Form1 : Form
@@ -43,12 +44,28 @@ namespace Capa_Conexion
             selectFrom = selectFrom + "  FROM [dbo].[Customers]";
 
             //-----------------------
+
             SqlCommand comando = new SqlCommand(selectFrom, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
+            List<Customers> Customers = new List<Customers>();
+
             while (reader.Read())
             {
-                var customerId = reader["CompanyName"];
+
+                Customers customers = new Customers();
+                customers.CompanyName = reader["CompanyName"] == DBNull.Value ? "" : (String)reader["CompanyName"];
+                customers.ContactName = reader["ContactName"] == DBNull.Value ? "" : (String)reader["ContactName"];
+                customers.ContactTitle = reader["ContactTitle"] == DBNull.Value ? "" : (String)reader["ContactTitle"];
+                customers.Address = reader["Address"] == DBNull.Value ? "" : (String)reader["Address"];
+                customers.City = reader["City"] == DBNull.Value ? "" : (String)reader["City"];
+                customers.Region = reader["Region"] == DBNull.Value ? "" : (String)reader["Region"];
+                customers.PostalCode = reader["PostalCode"] == DBNull.Value ? "" : (String)reader["PostalCode"];
+                customers.Country = reader["Country"] == DBNull.Value ? "" : (String)reader["Country"];
+                customers.Phone = reader["Phone"] == DBNull.Value ? "" : (String)reader["Phone"];
+                customers.Fax = reader["Fax"] == DBNull.Value ? "" : (String)reader["Fax"];
+
+            Customers.Add(customers);
             }
 
             MessageBox.Show("Conexion cerrada");
